f338f370493d9504e2a5c4b4c30f9b8d8c22cda2
ayoeze191 <ayoeze191@gmail.com>
2023-03-07 00:42:12 +0100

    asa

--- src/components/ui/Modal/BecomeAPartner.jsx
+++ src/components/ui/Modal/BecomeAPartner.jsx
@@ -22,32 +22,40 @@ const BecomeAPartner = () => {
   return (
     <div className='flex flex-col md:flex-row rounded-[10px 0px 0px 10px] text-center font-lato'>
         <div className='w-full md:w-[50%] bg-[#EFA30C] flex items-center justify-center'>
-
-        <div>
-            <img src={amico} alt="" />
+        <div className='flex gap-[6px] items-center py-[20px] px-[20px] md:px-[0] md:py-[0]'>
+        <div className='w-[120.97px] h-[136.12px] md:w-[initial] md:h-[initial]'>
+            <img src={amico} className='w-full h-full' alt="" />
+        </div>
+        <div className='font-[500] text-[12px] flex flex-col mt-[4px] mb-[11px] md:hidden'>
+        <p className='font-[700] font-lato text-[1.25rem] w-full md:fit'>Are you passionate about SDG projects for children?</p>
+                <p>Then fill the form below detailing your intentions</p>
+                    <p> OR </p>
+ 
+                <p> Shoot us a mail at <span className='text-[#5A0C91] mt-[4px]'> sales@ekopages.com </span></p>
+            </div>
         </div>
 
         </div>
         <div className='w-full md:w-[50%] bg-[#F9F9F9] px-[79px] py-[80px]'>
-            <p className='font-[700] font-lato text-[1.25rem] w-full md:fit'>Are you passionate about SDG projects for children?</p>
-            <div className='font-[500] text-[12px] flex flex-col gap-[1rem] mt-[4px] mb-[11px]'>
+            <p className='hidden md:block font-[700] font-lato text-[1.25rem] w-full md:fit'>Are you passionate about SDG projects for children?</p>
+            <div className='hidden font-[500] text-[12px] md:flex flex-col gap-[1rem] mt-[4px] mb-[11px]'>
                 <p>Then fill the form below detailing your intentions</p>
                     <p> OR </p>
  
                 <p> Shoot us a mail at <span className='text-[#5A0C91]'> sales@ekopages.com </span></p>
             </div>
 
-            <form action="" className='flex flex-col gap-[1.5rem] w-full md:w-fit'>
-            <section className="flex flex-col">
+            <form action="" className='flex flex-col gap-[1.5rem]  w-full'>
+            <section className="flex flex-col w-full">
             <InputField type="text" onChangeHandler={formik.handleChange} value={formik.values.username} fieldName={"Name"} name="name" id="Name" placeholder={"Enter your name here"}></InputField>
             {/* <p className="text-[12px] text-red-600 mt-4">{formik.touched.username && formik.errors.username ? formik.errors.username:null}</p> */}
             </section>
 
-            <section className="flex flex-col">
+            <section className="flex flex-col w-full">
             <InputField type="email" onChangeHandler={formik.handleChange} value={formik.values.email} fieldName={"Email Address"} name="email" id="email" placeholder={"example@gmail.com"}></InputField>
             {/* <p className="text-[12px] text-red-600 mt-4">{formik.touched.username && formik.errors.username ? formik.errors.username:null}</p> */}
             </section>
-            <section className='flex flex-col'>
+            <section className='flex flex-col w-full'>
                 <div className='font-[600] text-left text-[1rem] font-lato mb-[8px] '>Message</div>
             <textarea name="message" id="" cols="30" rows="10" placeholder='Type your message here' className='p-[1rem] border-[1px] border-solid border-[#CCB4DD] bg-[inherit] rounded-[2px]'> Type your message herer </textarea>
             </section>
