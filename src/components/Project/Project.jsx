199e42cc456f39f8f411571310096081c8e54944
ayoeze191 <ayoeze191@gmail.com>
2023-03-24 13:28:18 +0100

    shimmer

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import arrow from "./../../assets/projects/arrow.png"
 import { Link } from 'react-router-dom'
+import {Shimmer, Image} from "react-shimmer"
+
 const Project = ({img, h1, p, path}) => {
   return (
     <div className='flex flex-col font-lato'>
         <div className='mb-[1.5rem] '>
-            <img src={img} className=""/>
+            <Image src={img} className="" fallback={<Shimmer width={300} height={300} />}/>
         </div>
         <div className='flex flex-col gap-[0.5rem] mb-2.5rem'>
         <h1 className='font-lato font-[700] text-[24px]'>
