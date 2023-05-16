da0a7814f966fe4da2621a2599efb97dd3704dbb
ayoeze191 <ayoeze191@gmail.com>
2023-05-16 12:25:20 +0100

    asas

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import arrow from "./../../assets/projects/arrow.png"
 import { Link } from 'react-router-dom'
 import { Image} from "react-shimmer"
 import Shimmer from '../ui/Shimmer/Shimmer'
 
 const Project = ({img, h1, p, path}) => {
+    const [loaded, setLoaded] = useState(false) 
+
   return (
-    <div className='flex flex-col font-lato'>
+    <div className='flex flex-col font-lato w-full'>
         <div className='mb-[1.5rem] w-full h-[300px]'>
-            <Image src={img} className="" fallback={<Shimmer />}/>
+            {!loaded?<Shimmer />:null}
+            <img src={img} onLoad={() => setLoaded(true)}/>
         </div>
         <div className='flex flex-col gap-[0.5rem] mb-2.5rem'>
         <h1 className='font-lato font-[700] text-[24px]'>
