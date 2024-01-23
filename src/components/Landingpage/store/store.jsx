9d47f7661d76f2c16e923f42fb26c0ed3ad8c458
ayoeze191 <ayoeze191@gmail.com>
2024-01-23 23:16:52 +0100

    trying

--- src/components/Landingpage/store/store.jsx
+++ src/components/Landingpage/store/store.jsx
@@ -42,12 +42,14 @@ const Store = () => {
     disptach(get_products('All', "All"))
   }, [])
     return (
-        <div className=" section mb-[20px] font-lato  min-h-auto mt-[49.78px]">
+        <div className="  mb-[20px] font-lato  min-h-auto mt-[49.78px]">
             <p className="text-center text-[24px] md:text-[44px] font-[700] mb-[60px] mt-[57px]">Eko Store</p>
             {store.isloading ? <GeneralUiOverlayLoader /> :store.products?
             <div>
-    <div className='flex flex-col md:grid md:grid-cols-3 mx-auto section gap-x-[2rem] gap-y-10 md:gap-y-[5.25rem] items-center mb-[60px]'>
-        {store.products.slice(0, 6).map((prod) => <StoreCard {...prod}/>)}
+    <div className=" flex flex-col sm:grid md:grid-cols-2  lg:grid-cols-3 mx-auto section gap-x-[2rem] gap-y-[5.25rem] items-center mb-[60px]">
+      {store.products.slice(0, 6).map((prod) => (
+        <StoreCard {...prod} />
+      ))}
     </div>
     <div className=' w-full flex justify-center'>
     <button className='bg-[#5A0C91] text-[#E9E9E9] text-xl font-medium rounded-[5px]  font-lato px-[70px] py-[15px] mx-auto' onClick={() => navigate('/ekostore')}>
