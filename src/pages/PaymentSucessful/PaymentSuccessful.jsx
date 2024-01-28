9c31a8781257c07e7b5631bc981618779701ef5e
ayoeze191 <ayoeze191@gmail.com>
2024-01-28 23:06:49 +0100

    trying

--- src/pages/PaymentSucessful/PaymentSuccessful.jsx
+++ src/pages/PaymentSucessful/PaymentSuccessful.jsx
@@ -23,14 +23,14 @@ const PaymentSuccessful = () => {
     })
   return (
     verificationSuccesfull? <div>
-        <div className='flex justify-center flex-col items-center text-center gap-[29px]'>
-<div className='flex flex-col gap-[16px]'>
+        <div className='flex justify-center flex-col items-center text-center gap-[29px] mx-auto'>
+<div className='flex flex-col gap-[16px] mx-auto'>
  <p className='font-[700] font-lato text-[1.25rem] text-[#232323] md:text-[40px]'> We Have Received Your Payment.thanks four your patronage"</p>
  <p className='font-[400] font-lato text-[1rem] text-[#232323] md:text-[24px]'>
  Continue Using the app
  </p>
  </div>
- <button className='font-lato text-[1rem] text-[#E9E9E9] font-[500] bg-[#0d0c0e] py-[8px] px-[20px] rounded-[5px]' onClick={() => nav('/ekostore')}>
+ <button className='font-lato text-[1rem] text-[#E9E9E9] font-[500] bg-[#0d0c0e] py-[8px] px-[20px] rounded-[5px] w-fit mx-auto' onClick={() => nav('/ekostore')}>
  Continue
  </button>
  </div>
