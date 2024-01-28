3699248a059e8ee2c9aeec688ae5a552a2b38579
ayoeze191 <ayoeze191@gmail.com>
2024-01-28 23:02:40 +0100

    Asas

--- src/pages/PaymentSucessful/PaymentSuccessful.jsx
+++ src/pages/PaymentSucessful/PaymentSuccessful.jsx
@@ -35,13 +35,13 @@ const PaymentSuccessful = () => {
  </button>
  </div>
     </div>
-    :<><div className='flex flex-col gap-[16px]'>
+    :<><div className='flex flex-col gap-[16px] mx-auto'>
     <p className='font-[700] font-lato text-[1.25rem] text-[#232323] md:text-[40px]'> Opps!! we didnt Receive Your Payment</p>
     <p className='font-[400] font-lato text-[1rem] text-[#232323] md:text-[24px]'>
     Please visit the contact us modal for your complaint
     </p>
     </div>
-    <button className='font-lato text-[1rem] text-[#E9E9E9] font-[500] bg-[#0d0c0e] py-[8px] px-[20px] rounded-[5px]' onClick={() => nav('/ekostore')}>
+    <button className='font-lato text-[1rem] text-[#E9E9E9] font-[500] bg-[#0d0c0e] py-[8px] mx-auto px-[20px] rounded-[5px] w-fit' onClick={() => nav('/ekostore')}>
     Continue
     </button>
     </>
