7b2bcd158741067c7d97d4e069737772e91ed02a
ayoeze191 <ayoeze191@gmail.com>
2024-02-06 21:59:47 +0100

    trying

--- src/components/about/Partners.jsx
+++ src/components/about/Partners.jsx
@@ -5,7 +5,7 @@ import FME from "./../../assets/about/FME.png"
 import LME from "./../../assets/about/LME.png"
 import REESAfricaLogo from "./../../assets/about/REESAfricaLogo.png"
 import TNCI from "./../../assets/about/TNCI.png"
-import educator from "./../../assets/about/educators.jpeg"
+import educator from "./../../assets/about/educators.png"
 
 import partners from "./../../assets/landingpage/partners.svg"
 
@@ -29,7 +29,7 @@ const Partners = () => {
       {/* <div className=''><img src={partners} /></div> */}
         <h2 className='text-[#480A74] text-[24px] md:text-[44px] mx-auto w-fit mb-12'>Our Partners</h2>
         <Swiper 
-        // className=' flex w-full  md:w-fit justify-center items-center mx-auto '
+        className=' flex w-full  md:w-fit justify-center items-center mx-auto h-[150px] '
             // modules={[Navigation]}
             slidesPerView={1}
             spaceBetween= {0}
@@ -38,23 +38,31 @@ const Partners = () => {
             loop={true}
             breakpoints={{
               450: {
+                loop: true,
                 slidesPerView: 6,
                 spaceBetween: 5
               },
               640: {
+                loop: true,
                 slidesPerView: 6,
                 spaceBetween: 56
             }
         }}
         >
-            <SwiperSlide><div className='px-3 w-fit  mx-auto' ><img src={British} className='mx-auto w-40 md:w-full '/></div></SwiperSlide>
-            <SwiperSlide><div className='px-3 w-fit  mx-auto'  ><img src={chevening} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-            <SwiperSlide> <div className='px-3 w-fit   mx-auto'  ><img src={TNCI} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-            <SwiperSlide><div className='px-3 w-fit  mx-auto'  ><img src={FME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-            <SwiperSlide> <div className='px-3 w-fit  mx-auto'  ><img src={REESAfricaLogo} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-            <SwiperSlide><div className='px-3 w-fit mx-auto'  ><img src={LME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-            <SwiperSlide><div className='px-3 w-fit mx-auto'  ><img src={educator} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
-
+            <SwiperSlide><div className='px-3 w-full  mx-auto  flex items-center  h-full' ><img src={British} className='mx-auto w-40 md:w-full '/></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={chevening} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide> <div className='px-3 w-fit flex items-center   h-full   mx-auto'  ><img src={TNCI} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={FME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide> <div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={REESAfricaLogo} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit mx-auto flex items-center   h-full'  ><img src={LME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-[200px] mx-auto flex items-center   h-full'  ><img src={educator} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-full  mx-auto flex items-center   h-full' ><img src={British} className='mx-auto w-40 md:w-full '/></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={chevening} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide> <div className='px-3 w-fit   mx-auto flex items-center   h-full'  ><img src={TNCI} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={FME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide> <div className='px-3 w-fit  mx-auto flex items-center   h-full'  ><img src={REESAfricaLogo} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-fit mx-auto flex items-center   h-full'  ><img src={LME} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
+            <SwiperSlide><div className='px-3 w-[200px] mx-auto flex items-center   h-full'  ><img src={educator} className='mx-auto w-40 md:w-full' /></div></SwiperSlide>
             </Swiper>        
         </div>
     </div>
