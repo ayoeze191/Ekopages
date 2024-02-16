d886a435ccbddb1da4f6d5645154a0db0b06b0a6
ayoeze191 <ayoeze191@gmail.com>
2024-02-16 18:15:13 +0100

    clean-up

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { store } from './store/Store';
 import { ModalProvider } from './context/modal/modal';
 import ReactGA from 'react-ga';
 // Initialize React Ga with your tracking ID
-ReactGA.initialize('G-33MK520YP4');
+ReactGA.initialize('G-L30DJF68Y6');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 
