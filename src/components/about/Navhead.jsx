7f00f6ef07e8cfe501d99b4a5fd1938dda67cbc5
ayoeze191 <ayoeze191@gmail.com>
2024-03-01 03:52:53 +0100

    trying

--- src/components/about/Navhead.jsx
+++ src/components/about/Navhead.jsx
@@ -191,7 +191,7 @@ const NavHead = ({sideHandler}) => {
             <section className=' items-center justify-between gap-[1rem] flex flex-0'>
             
                  {isAuth && <>                      <p className="text-[#4A4A4A] text-[0.875rem] lg:text-[1rem] font-[400] ">hello {user.username}</p> 
-                    <Link className="  relative  w-[28px] h-[28px] lg:w-[40px] lg:h-[40px] bg-[#CCB4DD] rounded-full md:flex items-center justify-center cursor-pointer flex" to = {'/dashboard'} >
+                    <Link className="hidden  relative  w-[28px] h-[28px] lg:w-[40px] lg:h-[40px] bg-[#CCB4DD] rounded-full md:flex items-center justify-center cursor-pointer flex" to = {'/dashboard'} >
                         {/* <AiOutlineUser></AiOutlineUser> */}
                         <img src={profile_pics} className='object-center bg-center w-full h-full rounded-full'/>
                         <p className="absolute h-[7px] w-[7px] rounded-[7px] bg-[#03A800] top-[5px] right-[0px]"></p>
